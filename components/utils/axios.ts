fa8e790ae6cd397634c8f00c8b42f902c2c92438
Okereke Ifeanyi Vincent <vincentcode0@gmail.com>
2025-01-30 12:07:53 -0800

    Feat: Create axios file

---
+++ components/utils/axios.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance } from "axios";
+import { toast } from "react-toastify";
+
+const Axios: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+Axios.interceptors.request.use(
+  (config) => {
+    const token = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      toast.error("Unauthorized! Redirecting to login...");
+
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default Axios;
